088878726c929b2695af94c8015d21b149ec1c7d
Admantix <williamwijaya499@gmail.com>
2025-07-20 13:28:25 +0700

    fix: changes output directory from build to dir

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/SEA-Salon/",
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   plugins: [react()],
 });
